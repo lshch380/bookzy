41844e144a203741ee4dcad574c2554c275662c0
Ubuntu <lshchyahoocom@lshchyahoocom.5ypqxaifadluzgvvsh3q30wpxf.gx.internal.cloudapp.net>
2020-08-08 20:57:48 +0000

    first commit

---
+++ src/main/java/com/bookzy/bookzyweb/BookzyWebApplication.java
@@ -0,0 +1,13 @@
+package com.bookzy.bookzyweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookzyWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookzyWebApplication.class, args);
+	}
+
+}
